627f91429591f7e1952ef93bde1e0101e32d4847
bobd50 <galavu10@gmail.com>
2023-03-04 11:19:54 +0300

    first commit

---
+++ routes/user.js
@@ -0,0 +1,33 @@
+import express from "express";
+
+import {adminMiddleware, authMiddleware, requireSignin} from "../controllers/auth.js";
+
+import {
+    list,
+    photo,
+    read,
+    readForAdmin,
+    removeUser,
+    singleUpdate,
+    publicProfile,
+    safTest
+
+} from "../controllers/user.js";
+
+const router = express.Router()
+
+router.post('/callback', safTest);
+router.get('/user/profile', requireSignin, authMiddleware, read);
+router.get('/user/:username', publicProfile);
+
+router.put('/single-user/:_id', requireSignin, authMiddleware, singleUpdate);
+router.get('/user/photo/:username', photo);
+
+
+//admin routes
+router.get('/all-users', requireSignin, adminMiddleware, list);
+router.get('/single-user/:_id', requireSignin, adminMiddleware, readForAdmin);
+router.delete('/single-user/:_id', requireSignin, adminMiddleware, removeUser);
+router.put('/single-user/:_id', requireSignin, adminMiddleware, singleUpdate);
+
+export default router;
